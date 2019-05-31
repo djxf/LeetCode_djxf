0aae4fe4efe52a8566b90c1e2bdff75d59e83a9e
djxf <1763380121@1763380121@qq.com>
2019-05-31 18:42:49 +0800

    日常刷题

--- fnzb.js
+++ fnzb.js
@@ -14,78 +14,39 @@ var package_name = "me.ele.crowdsource";
 var cancle = "取消";
 var isOpen;             //记录打开蜂鸟是否成功
 
-//生成从minNum ~ maxNum的随机数
-function getrandomNum(minNum,maxNum){   
-    switch(arguments.length){ 
-        case 1: 
-            return parseInt(Math.random()*minNum+1,10); 
-        break; 
-        case 2: 
-            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
-        break; 
-            default: 
-                return 0; 
-            break; 
-    } 
-}
-
 //点击抢单函数
 function qdan(){
      //查找须设置超时时间否则陷入死循环 id = ia
-     var bt_helpBuy = id(qdId).findOne(100);
-     if(bt_helpBuy != null){
-        bt_helpBuy.click();
-        device.vibrate(50);
-        toast("已检测到抢单");
-    }
-     var bt_qd = id(qdId2).findOne(100);
-     if(bt_qd !=null){
-         bt_qd.click();
-         device.vibrate(50);
-         toast("已检测到抢单");
+     var bt_helpBuy = id(qdId).find(250);
+     for(var i=0;i<=bt_helpBuy.length-1;i++){
+         if(bt_helpBuy[i] !=null && bt_helpBuy[i].text != "待取货"){
+             bt_helpBuy[i].click();
+             device.vibrate(50);
+             toast("已检测到抢单");
+         }
      }
-    
+
+     var bt_qd = id(qdId2).find(250);
+     for(var i=0;i<=bt_qd.length-1;i++){
+        if(bt_qd[i] !=null && bt_qd[i].text != "确认送达"){
+            bt_qd[i].click();
+            device.vibrate(50);
+            toast("已检测到抢单");
+        }
+     }
 }
 
-//     //子线程点击确定 同时查找单子
-// threads.start(function(){
-//         while(true){
-//             //抢单
-//             qdan();
-//             //不进行判断直接点击确定
-//             click(ok);
-//             var re_cancle = text(cancle).findOne(50);//查找须设置超时时间否则陷入死循环
-//             if(re_cancle.text()=="取消"){
-//                 click(ok);
-//             }else{
-//                 log("未找到控件 但执行函数成功");
-//             }
-//         }
-// });
-
-
-    isOpen = app.launchPackage(package_name);
-    if(isOpen){
-        sleep(100);
-        toast(todo);
-    }
-    //主线程
-    while(true){
-            //抢单
-            qdan();
-            //直接点击确定
-            click(ok);
-            var re_cancle = text(cancle).findOne(50);
-            if(re_cancle!=null){
-                    click(ok);
-            }else{
-                    log("未找到控件 但执行函数成功");
-            }
-            //线程睡眠 延迟点击速度 防止被屏蔽
-            var randomtime = getrandomNum(5,20);
-            toast("本次暂停的随机时间："+ randomtime);
-            sleep(randomtime*1000);
-            click(refresh);
-            //抢单
-            qdan();
-    }
+ 
+isOpen = app.launchPackage(package_name);
+if(isOpen){
+    sleep(100);
+    toast(todo);
+}
+//主线程
+while(true){
+       //抢单
+       qdan();
+       //直接点击确定
+       click(ok);
+       var re_cancle = text(cancle).findOne(50);
+}
