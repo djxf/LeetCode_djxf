9b147ea6f76c709b6dfe011e5215aece37bec6bd
djxf <1763380121.qq.com>
2020-01-29 23:04:48 +0800

    二叉树路径和

---
+++ 112.路径总和.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=112 lang=java
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null){
+            return false;
+        }
+        if(root.left == null && root.right == null){
+            return sum - root.val == 0;
+        }
+    
+        return hasPathSum(root.left, sum-root.val)||hasPathSum(root.right, sum-root.val);
+    
+
+
+    }
+}
+// @lc code=end
+
