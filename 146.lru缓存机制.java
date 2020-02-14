965ea4f977bf1820a927c82fbde472106277eaf7
djxf <1763380121.qq.com>
2020-02-14 13:57:06 +0800

    LRU

--- 146.lru缓存机制.java
+++ 146.lru缓存机制.java
@@ -1,5 +1,4 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashMap;
 
 /*
  * @lc app=leetcode.cn id=146 lang=java
@@ -10,55 +9,100 @@ import java.util.Map;
 // @lc code=start
 class LRUCache {
 
-    private final LinkedHashMap<Integer,Integer> map;
-    private final int maxSize;
-    private int size = 0;
-    public LRUCache(final int capacity) {
-        if(capacity <= 0){
-            throw new IllegalArgumentException("capacity <= 0");
+    static class Node {
+        public int key,value;
+        public Node next,prev;
+        public Node(int k,int v){
+            this.key = k;
+            this.value = v;
         }
-        this.maxSize = capacity;
-        map = new LinkedHashMap<Integer,Integer>(0,0.75f,true);
+    }
+
+    static class DoubleList{
+        //头尾虚节点
+        private Node head,tail;
+        //链表元素数不包括头尾虚节点
+        private int size;
+        public DoubleList(){
+            head = new Node(0,0);
+            tail = new Node(0,0);
+            head.next = tail;
+            tail.prev = head;
+            size = 0;
+        }
+        
+        //在头部添加节点x
+        public void addFirst(Node node){
+            node.next = head.next;
+            node.prev = head;
+            head.next.prev = node;
+            head.next = node;
+            size++;
+        }
+
+        //删除链表中的x节点 （x存在？）
+        public void remove(Node node){
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            size--;
+        }
+
+        //删除链表中的最后一个节点 并返回该节点
+        public Node removeLast(){
+            if(tail.prev == head){
+                return null;
+            }
+            Node last = tail.prev;
+            remove(last);
+            return last;
+        }
+        
+        //返回链表长度
+        public int size(){
+            return size;
+        }
+
 
     }
+
+    private HashMap<Integer,Node> map;
+    private DoubleList cache;
+    private int maxSize;
+
+    public LRUCache(int capacity) {
+        this.maxSize = capacity;
+        map = new HashMap<>();
+        cache = new DoubleList();
+    }
     
-    public int get(int key) {
-        // if(Integer.toString(key) == null){
-        //     throw new IllegalArgumentException("key == null");
-        // }
-        int value = (int)map.get(key);
-        if(Integer.toString(value) != null){
-            //size--;
-            return value;
+    public int get(Integer key) {
+        if(!map.containsKey(key)){
+            return -1;
         }
-        return -1;
+
+        int  value = map.get(key).value;
+        put(key, value);//利用put方法将值提前
+        return value;
+
     }
     
     public void put(int key, int value) {
-        map.put(key,value);
-       
-        size++;
-
-        trimToSize(size);
-    }
-
-    public void trimToSize(int size){
-        int mSize = size;
-        while(mSize > maxSize){
-            Map.Entry<Integer, Integer> toEvict = null;
-                for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                    toEvict = entry;
-                }
-                // END LAYOUTLIB CHANGE
-
-                if (toEvict == null) {
-                    break;
-                }
-                int key = (int)toEvict.getKey();
-                map.remove(key);
-                mSize--;
+        Node node = new Node(key,value);
+        if(map.containsKey(key)){
+            cache.remove(map.get(key));
+            cache.addFirst(node);
+            map.put(key, node);
+        }else{
+            if(maxSize == cache.size){
+                Node last = cache.removeLast();
+                map.remove(last.key);
+            }
+            cache.addFirst(node);
+            map.put(key, node);
         }
     }
+
+   
 
 }
 
