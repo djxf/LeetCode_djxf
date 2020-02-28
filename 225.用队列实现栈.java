136922b10869a49ac286bc499bce5fd19632e15e
djxf <1763380121.qq.com>
2020-02-28 10:37:44 +0800

    队列实现栈

--- 225.用队列实现栈.java
+++ 225.用队列实现栈.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
  * @lc app=leetcode.cn id=225 lang=java
  *
@@ -8,28 +11,42 @@
 class MyStack {
 
     /** Initialize your data structure here. */
+
+    private Queue<Integer> q1 = new LinkedList<>();
+    private Queue<Integer> q2 = new LinkedList<>();
+    //保存栈顶元素
+    private int top;
     public MyStack() {
 
     }
     
     /** Push element x onto stack. */
     public void push(int x) {
-
+        q1.add(x);
+        top = x;
     }
     
     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-
+        while(q1.size() > 1){
+            top = q1.remove();
+            q2.add(top);
+        }
+        int result = q1.remove();
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+        return result;
     }
     
     /** Get the top element. */
     public int top() {
-
+        return top;
     }
     
     /** Returns whether the stack is empty. */
     public boolean empty() {
-
+        return q1.size() == 0;
     }
 }
 
